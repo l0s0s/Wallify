8ee2005465d85b8cb48f1ede9317e2a8882b33fe
Pavel Ogurtsov <cucumb3rpasha@gmail.com>
2024-06-03 11:57:19 +0300

    round background angles

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -7,19 +7,20 @@ const Grid = (wallpapers: Wallpaper[]) => {
             <div className="grid grid-cols-2 md:grid-cols-3 gap-4 container sm">
                 {wallpapers.map((wallpaper) => (
                     <div className="relative group">
-                        <a href={wallpaper.URL} target="_blank">
+                        <a href={wallpaper.URL} target="_blank" className="block">
                             <img
                                 className="h-auto max-w-full rounded-lg border border-gray-200"
                                 src={wallpaper.URL}
                             />
-                            <div className="overlay absolute bottom-0 left-0 w-full bg-black bg-opacity-50 text-white py-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center">
-                                <span>{wallpaper.Resolution}</span>
+                            <div className="overlay absolute bottom-0 left-0 w-full bg-black bg-opacity-50 py-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center rounded-tl-none rounded-tr-none rounded-bl-lg rounded-br-lg">
+                                <span className="text-white">{wallpaper.Resolution}</span>
                             </div>
                         </a>
                     </div>
                 ))}
             </div>
-        </div >
+        </div>
+
     );
 };
 
