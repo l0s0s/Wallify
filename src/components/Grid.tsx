9da5d0ed70a6e837cdc8fee873cf9804249feb5e
Pavel Ogurtsov <cucumb3rpasha@gmail.com>
2024-06-03 11:51:52 +0300

    show resolution

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -6,12 +6,15 @@ const Grid = (wallpapers: Wallpaper[]) => {
         <div className="center">
             <div className="grid grid-cols-2 md:grid-cols-3 gap-4 container sm">
                 {wallpapers.map((wallpaper) => (
-                    <div>
+                    <div className="relative group">
                         <a href={wallpaper.URL} target="_blank">
-                            < img
+                            <img
                                 className="h-auto max-w-full rounded-lg border border-gray-200"
                                 src={wallpaper.URL}
                             />
+                            <div className="overlay absolute bottom-0 left-0 w-full bg-black bg-opacity-50 text-white py-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-center">
+                                <span>{wallpaper.Resolution}</span>
+                            </div>
                         </a>
                     </div>
                 ))}
